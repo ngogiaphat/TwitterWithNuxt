4300d34e08b5d3afe37c16086f03e0111a5390bc
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-15 21:59:36 +0700

    added tweet deatials page

---
+++ server/db/mediaFiles.js
@@ -0,0 +1,6 @@
+import {prisma} from ".";
+export const createMediaFile = (mediaFileData) => {
+	return prisma.mediaFile.create({
+		data: mediaFile,
+	});
+};
---
+++ server/db/refreshToken.js
@@ -0,0 +1,20 @@
+import {prisma} from ".";
+export const createRefreshToken = (refreshToken) => {
+	return prisma.refreshToken.create({
+		data: refreshToken,
+	});
+};
+export const getRefreshTokenByToken = (token) => {
+	return prisma.refreshToken.findUnique({
+		where: {
+			token,
+		},
+	});
+};
+export const removeRefreshToken = (token) => {
+	return prisma.refreshToken.delete({
+		where: {
+			token,
+		},
+	});
+};
