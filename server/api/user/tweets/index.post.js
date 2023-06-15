cdab3a32b1b6a1f3ea7249308aaff1377e8faec8
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-15 18:14:01 +0700

    added tweet modal/reply modal

---
+++ server/api/user/tweets/index.post.js
@@ -0,0 +1,43 @@
+import formidable from 'formidable';
+import {createTweet} from '../../../db/tweets.js';
+import {createMediaFile} from '../../../db/mediaFiles.js';
+import {uploadToCloudinary} from '../../../utils/cloudinary.js';
+import { tweetTransformer } from '~~/server/transformers/tweet.js';
+export default defineEventHandler(async(event) => {
+	const form = formidable({
+		multiples: true
+	});
+	const response = await new Promise((resolve, reject) => {
+		form.parse(event.req, async(err, fields, files) => {
+			if(err){
+				reject(err);
+			};
+			resolve({fields, files});
+		});
+	});
+	const {fields, files} = response;
+	const userId = event.context?.auth?.user?.id;
+	const tweetData = {
+		text: fields.text,
+		authorId: userId,
+	};
+	const replyTo = fields.replyTo;
+	if(replyTo && replyTo !== 'null' && replyTo !== 'undefined'){
+		tweetData.replyToId = replyTo;
+	};
+	const tweet = await createTweet(tweetData);
+	const filePromise = Object.keys(files).map(async(key) => {
+		const file = files[key];
+		const cloudinaryResource = await uploadToCloudinary(file.filepath);
+		return createMediaFile({
+			url: cloudinaryResource.secure_url,
+			providerPublicId: cloudinaryResource.public_id,
+			userId: userId,
+			tweetId: tweet.id,
+		});	
+	});
+	await Promise.all(filePromise);
+	return {
+		tweet: tweetTransformer(tweet),
+	};
+});
