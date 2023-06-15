f71be6937a88de85b0dd95d50134920b6ae91701
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-15 16:23:40 +0700

    added login and logout

---
+++ composables/useAuth.js
@@ -0,0 +1,111 @@
+import jwt_decode from "jwt-decode";
+export default() => {
+	const useAuthToken = () => useState("auth_token");
+	const useAuthUser = () => useState("auth_user");
+	const useAuthLoading = () => useState("auth_loading", () => true);
+	const setToken = (newToken) => {
+		const authToken = useAuthToken();
+		authToken.value = newToken;
+	};
+	const setUser = (newUser) => {
+		const authUser = useAuthUser();
+		authUser.value = newUser;
+	};
+	const setIsAuthLoading = (value) => {
+		const authLoading = useAuthLoading();
+		authLoading.value = value;
+	};
+	const login = ({username, password}) => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				const data = await $fetch("api/auth/login", {
+					method: "POST",
+					body: {
+						username,
+						password,
+					},
+				});
+				setToken(data.access_token);
+				setUser(data.user);
+				resolve(true);
+			}
+			catch(error){
+				reject(error);
+			};
+		});
+	};
+	const refreshToken = () => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				const data = await $fetch("api/auth/refresh");
+				setToken(data.access_token);
+				resolve(true);
+			}
+			catch(error){
+				reject(error);
+			};
+		});
+	};
+	const getUser = () => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				const data = await userFetchApi("api/auth/user");
+				setUser(data.user);
+				resolve(true);
+			}
+			catch(error){
+				reject(error);
+			};
+		});
+	};
+	const reRefreshToken = () => {
+		const authToken = useAuthToken();
+		if(!authToken.value){
+			return;
+		};
+		const jwt = jwt_decode(authToken.value);
+		const newRefreshTime = jwt.exp - 60000;
+		setTimeout(async() => {
+			await reRefreshToken();
+			reRefreshAccessToken();
+		}, newRefreshTime);
+	};
+	const initAuth = () => {
+		return new Promise(async(resolve, reject) => {
+			setIsAuthLoading(true);
+			try {
+				await reRefreshToken();
+				await getUser();
+				reRefreshToken();
+				resolve(true);
+			}
+			catch(error){
+				console.log(error);
+				reject(error);
+			}
+			finally {
+				setIsAuthLoading(false);
+			};
+		});
+	};
+	const logout = () => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				await userFetchApi("api/auth/logout", {
+					method: "POST",
+				});
+			}
+			catch(error){
+				reject(error);
+			};
+		});
+	};
+	return {
+		login,
+		useAuthUser,
+		useAuthToken,
+		initAuth,
+		useAuthLoading,
+		logout,
+	};
+};
